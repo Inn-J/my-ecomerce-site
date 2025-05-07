edcab1a3c152a356cf29b7f57f20f9af150f72ea
Inn-J <inn.javanicha@gmail.com>
2025-05-07 16:34:39 +0700

    Session 5

---
+++ fileServer.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer(function (req, res) {
+    //var q = url.parsez(req.url,true)
+
+  let filePath = '.'+req.url;
+  if(filePath === "./") filePath = "./index.html";
+  
+  const extname = path.extname(filePath);
+  let contentType = "Text/html";
+  if(extname === ".css") contentType = "Text/css";
+  if(extname === ".xml") contentType = "Text/xml";
+  if(extname === ".js") contentType = "appication/js";
+  
+  fs.readFile(filePath,function(err,htmlDoc){
+    if(err){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        return res.end("404 : Not found!!!!!");
+    }
+    res.writeHead(200, {'Content-Type': contentType});
+    var q = url.parse(req.url,true).query;
+    var txt = q.fname+ ' '+q.lname;
+    res.write(htmlDoc);
+    res.end();
+  });
+  
+});
+
+server.listen(8080);
